f4f33b2dd0f7c34e24cf812ff07eafae4177e677
Christina Mackiewicz <christina.mackiewicz@gmail.com>
2022-05-22 14:06:39 -0600

    add form example

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,16 @@ app = Flask(__name__)
 def index():
     data = {'message':'it works'}
     return render_template('index.html', data = data)
+
+@app.route("/hello_form")
+def hello_form():
+    return render_template('hello_form.html', data=None)
     
+@app.route("/hello_results", methods=["POST"])
+def hello_results():
+    data = {'message': request.form.get('name')}
+    return render_template('hello_results.html', data=data)
+   
+
 if __name__=='__main__':
     app.run()
